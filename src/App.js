6c018f267afa636cb8856469f84249eba9e04ae9
temptado <jason1287712@gmail.com>
2023-02-24 05:25:03 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <>Hello World</>
+    <Router>
+      <Routes>
+        <Route path='/'></Route>
+        <Route path='/about'></Route>
+        <Route path='/services'></Route>
+        <Route path='/contact'></Route>
+      </Routes>
+    </Router>
   );
 }
 
