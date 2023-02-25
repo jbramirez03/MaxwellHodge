33036a7928552cc6843eab1858fb0e73a31468c0
temptado <jason1287712@gmail.com>
2023-02-25 16:26:37 -0500

    added nav component elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className='header' id='header'>
+            <nav className='nav container'>
+                <Link to={'/'} className='logo nav__logo'>MaxwellHodge</Link>
+                <div className="nav__menu" id='nav-menu'>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <Link className='nav__link' to='/'>Home</Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link className='nav__link' to='/about'>About</Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link className="nav__link" to='/services'>Services</Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link className="nav__link" to='/contact'>Contact</Link>
+                        </li>
+                    </ul>
+
+                    <i className="ri-close-line nav__close" id='nav-close'></i>
+                </div>
+
+                <div className="nav__toggle">
+                    <i className="ri-function-line"></i>
+                </div>
+            </nav>
+        </header>
     )
 }
 
