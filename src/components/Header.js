c2b23fd9fb1b410ca3cff22eac586554c788db6b
temptado <jason1287712@gmail.com>
2023-02-25 17:06:46 -0500

    added collapsible header for mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,27 @@ import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+
+    const navLink = document.querySelectorAll('.nav__link')
+    const [menuState, setMenuState] = React.useState(false);
+    const [scrollState, setScrollState] = React.useState(false);
+
+    function linkAction() {
+        setMenuState(false);
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
+
+    function scrollHeader() {
+        if (this.scrollY >= 80) setScrollState(true);
+        else setScrollState(false);
+    }
+    window.addEventListener('scroll', scrollHeader);
+
     return (
-        <header className='header' id='header'>
+        <header className={`header ${scrollState ? 'scroll-header' : ''}`} id='header'>
             <nav className='nav container'>
                 <Link to={'/'} className='logo nav__logo'>MaxwellHodge</Link>
-                <div className="nav__menu" id='nav-menu'>
+                <div className={`nav__menu ${menuState ? 'show-menu' : ''}`} id='nav-menu'>
                     <ul className="nav__list">
                         <li className="nav__item">
                             <Link className='nav__link' to='/'>Home</Link>
@@ -23,10 +39,10 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <i className="ri-close-line nav__close" id='nav-close'></i>
+                    <i className="ri-close-line nav__close" onClick={() => setMenuState(false)} id='nav-close'></i>
                 </div>
 
-                <div className="nav__toggle">
+                <div className='nav__toggle' id="nav-toggle" onClick={() => setMenuState(true)}>
                     <i className="ri-function-line"></i>
                 </div>
             </nav>
