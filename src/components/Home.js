1c1ba3bf5c6fadf289bff691474e705321bc845c
temptado <jason1287712@gmail.com>
2023-02-27 22:17:40 -0500

    added styling to home image container and home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ const Home = () => {
     return (
         <div className='home__section section container' id='home'>
             <div className='home__background'></div>
-            <img src={homeImg} />
+            <div className="home__image-container">
+                <img src={homeImg} />
+                <h1 className=''>Maxwell Hodge</h1>
+            </div>
         </div>
     )
 }
