2f5d7347ea0601fbcf021b04cd63e8033ecc2f24
temptado <jason1287712@gmail.com>
2023-02-27 22:05:08 -0500

    added home image with background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import homeImg from '../images/main.png';
 
 const Home = () => {
     return (
         <div className='home__section section container' id='home'>
-
+            <div className='home__background'></div>
+            <img src={homeImg} />
         </div>
     )
 }
