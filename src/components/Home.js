a69930656932db5a7fee597ce0af88c459096713
temptado <jason1287712@gmail.com>
2023-02-28 17:58:41 -0500

    cleaned up styling code

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import homeImg from '../images/main.png';
 const Home = () => {
     return (
         <div className='home__section section container' id='home'>
-            <div className='vl-home vl-1' />
-            <div className='vl-home vl-2' />
-            <div className='vl-home vl-3' />
-            <div className='vl-home vl-4' />
+            <div className='vl-home vl vl-1' />
+            <div className='vl-home vl vl-2' />
+            <div className='vl-home vl vl-3' />
+            <div className='vl-home vl vl-4' />
 
             <div className="home__image-container">
                 <img src={homeImg} />
-                <div className="line__one">
+                <div className="home__line-one">
                     <h1 className='home__title'>Maxwell</h1>
                 </div>
-                <div className="line__two">
+                <div className="home__line-two">
                     <h1 className='home__title'>Hodge</h1>
                 </div>
                 <button className="home__button">Reach Me</button>
