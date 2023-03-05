96c0275d2e520fe85f131c98c8f58341414f5fe3
temptado <jason1287712@gmail.com>
2023-03-05 16:24:38 -0500

    added responsive view for about section desktop view

--- src/components/About.js
+++ src/components/About.js
@@ -10,19 +10,22 @@ const About = () => {
 
     return (
         <div className='about__section section container' id='about'>
-            <div className="about__image-container">
-                <img src={aboutImg} alt="Potraitsss" />
-            </div>
-            <div className="about__details">
-                <h3 className="about__title">Real Estate Agent</h3>
-                <h1 className="about__subtitle">About Me...</h1>
-                <div className="about__socials">
-                    <i className="ri-instagram-line"></i>
-                    <i className="ri-facebook-line"></i>
-                    <i className="ri-mail-line"></i>
+            <div className="about__content">
+
+                <div className="about__image-container">
+                    <img src={aboutImg} alt="Potraitsss" />
                 </div>
-                <p className="about__description">Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.</p>
-                <button className="about__button">Learn More</button>
+                <div className="about__details">
+                    <h3 className="about__title">Real Estate Agent</h3>
+                    <h1 className="about__subtitle">About Me...</h1>
+                    <div className="about__socials">
+                        <i className="ri-instagram-line"></i>
+                        <i className="ri-facebook-line"></i>
+                        <i className="ri-mail-line"></i>
+                    </div>
+                    <p className="about__description">Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.</p>
+                    <button className="about__button">Learn More</button>
+                </div>
             </div>
             <Swiper
                 className='mySwiper'
