9335d829d1905b188caf5f9d6f47b2e291cc7905
temptado <jason1287712@gmail.com>
2023-03-06 22:12:27 -0500

    added contact form elements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,13 +6,34 @@ const Contact = () => {
             <div className="contact__container">
                 <div className="contact__left">
                     <h1 className="contact__title">Get in touch</h1>
-                    <form className="contact__form"></form>
+                    <h2 className="contact__subtitle">Send a Message</h2>
+                    <form className="contact__form" method='POST' action=''>
+                        <div className="contact__inputs">
+                            <div className="contact__content">
+                                <input type="email" name='email' id='email' placeholder=' ' className="contact__input" />
+                                <label htmlFor="email" className='contact__label'>Email</label>
+                            </div>
+                            <div className="contact__content">
+                                <input type="text" name='subject' id='subject' placeholder=' ' className="contact__input" />
+                                <label htmlFor="subject" className='contact__label'>Subject</label>
+                            </div>
+                            <div className="contact__content contact__area">
+                                <textarea name="message" id="message" placeholder=' ' className='contact__input' data-gramm_editor="false"
+                                    data-enable-grammarly="false"></textarea>
+                                <label htmlFor="message" className='contact__label'>Message</label>
+                            </div>
+                        </div>
+                        <button className="button button--flex" action='submit'>
+                            Send Message
+                            <i className="ri-arrow-right-up-line button__icon"></i>
+                        </button>
+                    </form>
                 </div>
                 <div className="contact__options"></div>
             </div>
             <div className="contact__faq">
                 <div className="faq__titles">
-                    <h3 className="faq__small"></h3>
+                    <h3 className="faq__small"></h3>m
                     <h1 className="faq__big"></h1>
                 </div>
 
