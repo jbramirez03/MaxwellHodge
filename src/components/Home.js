aae2f824b0a76817795ab1269e3ac4b067ef1c83
temptado <jason1287712@gmail.com>
2023-03-11 21:39:22 -0500

    added form to home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,6 +22,29 @@ const Home = () => {
                     <button className="home__button">Reach Me</button>
                 </div>
             </div>
+            <div className="home__form-container">
+                <form className="home__form" method='POST' action=''>
+                    <div className="home__inputs">
+                        <div className="home__content">
+                            <input type="email" name='email' id='email' placeholder=' ' className="home__input" />
+                            <label htmlFor="email" className='home__label'>Email</label>
+                        </div>
+                        <div className="home__content">
+                            <input type="text" name='subject' id='subject' placeholder=' ' className="home__input" />
+                            <label htmlFor="subject" className='home__label'>Subject</label>
+                        </div>
+                        <div className="home__content home__area">
+                            <textarea name="message" id="message" placeholder=' ' className='home__input' data-gramm_editor="false"
+                                data-enable-grammarly="false"></textarea>
+                            <label htmlFor="message" className='home__label'>Message</label>
+                        </div>
+                    </div>
+                    <button className="home__button" action='submit'>
+                        Send Message
+                        <i className="ri-arrow-right-up-line button__icon"></i>
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
